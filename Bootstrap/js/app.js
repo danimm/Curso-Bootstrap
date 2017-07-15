7c7a092c71263cd9d063798ccfd6c49ccd3497e9
danimm <danimm1989@gmail.com>
2017-07-15 20:59:39 +0200

    Añadiendo el catalogo

---
+++ Bootstrap/js/app.js
@@ -0,0 +1,35 @@
+const $form = $('#suscribeForm')
+const $formGroup = $form.find('.form-group')
+const $formControl = $formGroup.find('.form-control')
+
+$form.submit(function (ev) {
+  // alert('Hola')
+  ev.preventDefault()
+
+  const error = Math.random() > 0.5
+  $formGroup.removeClass().addClass('form-group')
+  $formControl.removeClass().addClass('form-control')
+  $formGroup.addClass( error ? 'has-danger' : 'has-success')
+  $formControl.addClass( error ? 'form-control-danger' : 'form-control-success')
+  $formGroup.find('.form-control-feedback').remove()
+  var el;
+  if (error) {
+    el = $('<div>', { html: 'Ha ocurrido un error' })
+  } else {
+    el = $('<div>', { html: 'Te enviaremos todas las novedades' })
+  }
+  el.addClass('form-control-feedback')
+  $formGroup.append(el)
+
+})
+// $('modaloferta').modal({backdrop: 'static', keyboard: false})
+function mostrarModal(){
+  const noMostrarModal = JSON.parse(localStorage.noMostrarModal)
+  if (!noMostrarModal) {
+    $('#modalOferta').modal()
+  }
+
+  $('#btnNoRegistrar').click(function (ev){
+    localStorage.noMostrarModal = true
+  })
+}
